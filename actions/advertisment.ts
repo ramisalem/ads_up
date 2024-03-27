7e45bfe4890ee1dd1c73a00b49ea6632ba284ee2
Alaa Alser <alaalsser@gmail.com>
2024-03-27 14:16:26 +0200

    convet fetch to axios

--- actions/advertisment.ts
+++ actions/advertisment.ts
@@ -1,31 +1,37 @@
-"use server";
-
+import axios from "axios";
 import { AdvType } from "@/constants/types";
 
-export const getAllAds = async (): Promise<AdvType[] | string | undefined | any> => {
+export const getAllAds = async (): Promise<{
+  data?: AdvType[];
+  isError: boolean;
+  error: string;
+}> => {
+  let data: AdvType[] | undefined;
+  let isError = false;
+  let error = "";
 
-    let data: [AdvType] | undefined;
-    let isError = false;
-    let error = "";
-    try {
-        const response = await fetch("https://65f372c4105614e654a089c4.mockapi.io/api/v1/ads", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        const res = await response.json()
-
-        data = res
-
-
-    } catch (e) {
-        isError = true;
-        if (typeof e === "string") error = e;
-        else if (e instanceof Error) error = e.message;
-        else error = "Error";
+  try {
+    const response = await axios.get(
+      "https://65f372c4105614e654a089c4.mockapi.io/api/v1/ads",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    data = response.data;
+  } catch (e: any) {
+    isError = true;
+    if (e.response) {
+      error = `Error ${e.response.status}: ${e.response.data}`;
+    } else if (e.request) {
+      // The request was made but no response was received
+      error = "No response received from server.";
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      error = `Error: ${e.message}`;
     }
+  }
 
-    return { data, isError, error };
-
-}
+  return { data, isError, error };
+};
--- actions/helpcenter.ts
+++ actions/helpcenter.ts
@@ -1,10 +1,14 @@
-"use server";
+import axios from "axios";
 import { Tickets } from "@/constants/types";
 
 export const getTickets = async (): Promise<Tickets[]> => {
-    const res = await fetch(
-        "https://65f372c4105614e654a089c4.mockapi.io/api/v1/tickets"
+  try {
+    const response = await axios.get(
+      "https://65f372c4105614e654a089c4.mockapi.io/api/v1/tickets"
     );
-    const data = await res.json();
-    return data;
-}
+    return response.data;
+  } catch (error) {
+    console.error("There was a problem with your Axios request:", error);
+    throw error;
+  }
+};
